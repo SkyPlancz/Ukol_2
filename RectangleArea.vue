0e88b0b8d95380ac4a12a1d5cff420fafddaf22f
SkyPlancz <info@skyplan.cz>
2025-12-29 17:56:02 +0100

    Add Vue SFC rectangle area with computed

---
+++ RectangleArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>Delka obdelníku je {{ delka }}, šířka je {{ sirka }} a obsah je {{ obsah }}.</p>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+  name: 'RectangleArea',
+  setup() {
+    const delka = ref(Math.floor(Math.random() * 10));
+    const sirka = ref(Math.floor(Math.random() * 10));
+
+    const obsah = computed(() => delka.value * sirka.value);
+
+    return { delka, sirka, obsah };
+  },
+};
+</script>
+
+<style scoped>
+p { font-family: "Segoe UI", Tahoma, sans-serif; }
+</style>
